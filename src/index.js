59f8560da242e09fff826c06cd01751e4130bc1e
am1macdonald <am1macdonald@gmail.com>
2022-03-16 17:12:17 -0400

    update test scripts in lock file && update imports in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
-import './stylesheets/reset.css';
-import './stylesheets/style.sass';
-import './scripts/displayControls.js';
-import './scripts/game';
+import "./stylesheets/reset.css";
+import "./stylesheets/style.sass";
