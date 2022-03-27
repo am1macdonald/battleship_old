b5829c7701f860a3c6ed2215248e6ce5c288e938
am1macdonald <am1macdonald@gmail.com>
2022-03-26 22:42:31 -0400

    add function to bind listeners && function with game start instructions

--- src/scripts/gameLoop.js
+++ src/scripts/gameLoop.js
@@ -1,15 +1,54 @@
 import { gameArea } from "./DOMControls";
 import newGame from "./factories/gameFactory";
 
-const bindListeners = () => {
-  console.log("stuff");
-};
-
 let game;
 
+
+// only useful until two player games are possible
+const bindListeners = (playerOneName, playerTwoName = null) => {
+  const playerOneSquares = [
+    ...document.querySelector(`[data-owner="${playerOneName}"]`).children,
+  ];
+  const playerShipDrawer = document.querySelectorAll(`[data-ship]`);
+  console.log(playerShipDrawer);
+  const playerTwoSquares = [
+    ...document.querySelector('[data-owner="Player Two"]').children,
+  ];
+
+
+  // binds event listeners for player ship setup
+  if (game.getStage() === "setup") {
+    playerOneSquares.forEach((square) => {
+      square.addEventListener("click", () => {
+        const data = {
+          owner: square.parentElement.dataset.owner,
+          coord: square.dataset.coord,
+        };
+        game.eventManager(data)
+      });
+    });
+    console.log("player selection bound");
+
+
+    // binds event listeners for player to make attacks
+  } else if (game.getStage() === "gameplay") {
+    playerTwoSquares.forEach((square) => {
+      square.addEventListener("click", () => {
+        const data = {
+          owner: square.parentElement.dataset.owner,
+          coord: square.dataset.coord,
+        };
+        game.eventManager(data)
+      });
+    });
+    console.log("player attack selection bound");
+  }
+};
+
 const startGame = (playerOneName, playerTwoName = null) => {
-  gameArea(playerOneName, "Computer Player");
+  gameArea(playerOneName, "Player Two");
   game = newGame(playerOneName, playerTwoName);
+  bindListeners(playerOneName, playerTwoName);
 };
 
 export default startGame;
